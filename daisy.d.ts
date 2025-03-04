cb798b927299034594d77501e21e55ccbfb4da78
Shahmar <shahmar.dennis@yahoo.com>
2025-03-04 15:43:20 -0600

    Added typescript and editing file to include better version calling

--- daisy.d.ts
+++ daisy.d.ts
@@ -2,3 +2,8 @@ declare module 'daisyui' {
     const daisyui: any;
     export default daisyui;
   }
+
+  declare module 'daisyui/imports.js' {
+    const imports: any;
+    export default imports;
+  }
